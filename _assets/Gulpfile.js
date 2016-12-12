f587fb0c90993384b9d4c26a7cf7d1b431d2f965
aabereu <amando.abreu@trivago.com>
2016-12-12 16:32:33 +0000

    Added gulpfile with s3 upload for images

--- _assets/Gulpfile.js
+++ _assets/Gulpfile.js
@@ -7,6 +7,38 @@ const autoprefixer = require('gulp-autoprefixer');
 const scssFiles = 'scss/**/*.scss';
 const cssFiles = 'css/**/*.css';
 const uglify = require('gulp-uglifyjs');
+var awspublish = require('gulp-awspublish');
+const fs = require("fs");
+
+var config = JSON.parse(fs.readFileSync('./../private/accesskeys.json'));
+
+gulp.task('publish', function() {
+
+    // create a new publisher using S3 options
+    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+    var publisher = awspublish.create(
+        config, {
+            cacheFileName: './awscache'
+        });
+
+    // define custom headers
+    var headers = {
+        'Cache-Control': 'max-age=315360000, no-transform, public'
+        // ...
+    };
+
+    return gulp.src('./images/**/*.(css|png|jpg)')
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+        .pipe(publisher.publish(headers))
+
+        // create a cache file to speed up consecutive uploads
+        .pipe(publisher.cache())
+
+        // print upload updates to console
+        .pipe(awspublish.reporter());
+});
 
 
 var sassOptions = {
@@ -28,10 +60,10 @@ gulp.task('css', function() {
     gulp.src(['fonts/**/*']).pipe(gulp.dest('../assets/fonts'));
 
     gulp.src([scssFiles])
-    .pipe(sass(sassOptions))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('../assets/css'));
+        .pipe(sass(sassOptions))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('../assets/css'));
 });
 
 gulp.task('js', function() {
@@ -41,7 +73,8 @@ gulp.task('js', function() {
         .pipe(gulp.dest('../assets/js'))
 });
 
-gulp.task('default', ['css', 'js'], function() {
+gulp.task('default', ['css', 'js', 'publish'], function() {
     gulp.watch('scss/**/*.scss',['css']);
     gulp.watch('js/**/*.js', ['js']);
+    gulp.watch('images/**/**/*.(css|png|jpg)', ['publish']);
 });
